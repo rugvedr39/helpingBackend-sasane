e8b04474e1993f3e0c13e3b1ef5db15d905dd69d
Rugved-Technify <rugvedr39@gmail.com>
2024-05-14 19:53:54 +0530

    added tsconfig
    added tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import app from './app';
+import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, '0.0.0.0', function() {
-    console.log('Listening to port:  ' + 3000);
+app.listen(PORT, () => {
+  console.log("Listening to port:  " + 3000);
 });
