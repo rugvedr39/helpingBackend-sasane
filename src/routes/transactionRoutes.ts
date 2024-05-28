5fd463e107adf8df981520a22e2948b80114e64f
Rugved-Technify <rugvedr39@gmail.com>
2024-05-28 18:54:23 +0530

    new minor changes
    new minor changes

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -6,6 +6,7 @@ import {
   TransactionComplete,
   getReferralTree,
 } from "../controllers/giveHelpController";
+const { Op } = require("sequelize");
 import { GiveHelp } from "../models/give_help";
 import { sequelize } from "../config/database";
 const router = express.Router();
@@ -20,6 +21,10 @@ router.get("/top-receivers", async (req, res) => {
     const topReceivers = await GiveHelp.findAll({
       where: {
         status: "Completed",
+        [Op.ne]: [
+          5, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
+          192,
+        ],
       },
       attributes: [
         "receiver_id",
